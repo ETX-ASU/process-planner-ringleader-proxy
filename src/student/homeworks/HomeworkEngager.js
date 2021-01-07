ec76ee855d36ceb54b7834661ddf654ae207a058
Artur Wróbel <artur.gtg.wrobel@gmail.com>
2021-01-07 11:44:10 +0100

    fix refreshing data for homepage review

--- src/student/homeworks/HomeworkEngager.js
+++ src/student/homeworks/HomeworkEngager.js
@@ -84,8 +84,8 @@ function HomeworkEngager(props) {
 
   async function closeModalAndReview() {
     setActiveModal(null);
-    dispatch(setActiveUiScreenMode(UI_SCREEN_MODES.reviewHomework));
     await props.refreshHandler();
+    dispatch(setActiveUiScreenMode(UI_SCREEN_MODES.reviewHomework));
   }
 
   function handleHomeworkDataChange(data) {
