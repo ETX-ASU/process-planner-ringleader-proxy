33068aa7fa883f337d4bd84d514398f6fe03c204
Artur Wróbel <artur.gtg.wrobel@gmail.com>
2021-05-14 12:02:08 +0200

    feat: reorganize for for defining access levels

--- src/tool/components/Config/Config.js
+++ src/tool/components/Config/Config.js
@@ -2,7 +2,7 @@ import React, { useCallback } from "react";
 import { Container, Col, Form, Row } from "react-bootstrap";
 import { useProcessPlanner } from "../../hooks/useProcessPlanner";
 import { ACCESS_LEVELS } from "../../constants";
-import { AccessLevelHelp } from "../AccessLevel/AccessLevelHelp";
+import styles from "./Config.module.scss";
 
 export const Config = () => {
   const { plannerInfo, setPlannerInfo } = useProcessPlanner();
@@ -19,89 +19,134 @@ export const Config = () => {
     [setPlannerInfo]
   );
 
+  const handleCheckboxChange = useCallback(
+    (event) => {
+      const { name, checked } = event.target;
+
+      if (name === ACCESS_LEVELS.full && checked) {
+        handlePlannerUpdate("studentAccessLevel", ACCESS_LEVELS.full);
+      }
+
+      if (name === ACCESS_LEVELS.full && !checked) {
+        handlePlannerUpdate("studentAccessLevel", ACCESS_LEVELS.partial);
+      }
+
+      if (name === ACCESS_LEVELS.partial && checked) {
+        handlePlannerUpdate("studentAccessLevel", ACCESS_LEVELS.partial);
+      }
+
+      if (name === ACCESS_LEVELS.partial && !checked) {
+        handlePlannerUpdate("studentAccessLevel", ACCESS_LEVELS.readonly);
+      }
+    },
+    [handlePlannerUpdate]
+  );
+
   if (!plannerInfo) {
     return null;
   }
 
-  const levelLabels = {
-    full: "Full",
-    partial: "Parial",
-    readonly: "Read only",
-  };
-
   return (
-    <Container className="mt-2 ml-2 mr-2 mb-4">
-      <Form>
-        <Row>
-          <Form.Group as={Col}>
-            <Form.Label>Maximum score for the assignment</Form.Label>
-            <Form.Control
-              type="number"
-              value={plannerInfo.maxScore}
-              onChange={(event) =>
-                handlePlannerUpdate(
-                  "maxScore",
-                  event.target.value ? parseInt(event.target.value) : ""
-                )
-              }
-            />
-          </Form.Group>
-          <Form.Group as={Col}>
-            <Form.Label>
-              Student access level
-              <AccessLevelHelp />
-            </Form.Label>
-            <div>
-              {Object.keys(ACCESS_LEVELS).map((level) => (
-                <Form.Check
-                  inline
-                  key={level}
-                  type="radio"
-                  id={`student-edit-level-${level}`}
-                  name="student-edit-level"
-                  label={levelLabels[level]}
-                  checked={plannerInfo.studentAccessLevel === level}
-                  value={level}
+    <Form>
+      <Container className="mt-2 ml-2 mr-2 mb-4">
+        <Row className={styles.row}>
+          <Col className={styles.column}>
+            <h3>Score Settings</h3>
+            <div className={styles.content}>
+              <Form.Group>
+                <Form.Label>Maximum score for the assignment</Form.Label>
+                <Form.Control
+                  type="number"
+                  value={plannerInfo.maxScore}
                   onChange={(event) =>
                     handlePlannerUpdate(
-                      "studentAccessLevel",
-                      event.target.value
+                      "maxScore",
+                      event.target.value ? parseInt(event.target.value) : ""
                     )
                   }
                 />
-              ))}
+              </Form.Group>
+              <Form.Group>
+                <Form.Label>
+                  Required minimum word count in text section
+                </Form.Label>
+                <Form.Control
+                  type="number"
+                  value={plannerInfo.minWordsCount}
+                  onChange={(event) =>
+                    handlePlannerUpdate(
+                      "minWordsCount",
+                      event.target.value ? parseInt(event.target.value) : ""
+                    )
+                  }
+                />
+              </Form.Group>
+              <Form.Group>
+                <Form.Label>
+                  Required minimum items in checklist section
+                </Form.Label>
+                <Form.Control
+                  type="number"
+                  value={plannerInfo.minChecklistItems}
+                  onChange={(event) =>
+                    handlePlannerUpdate(
+                      "minChecklistItems",
+                      event.target.value ? parseInt(event.target.value) : ""
+                    )
+                  }
+                />
+              </Form.Group>
             </div>
-          </Form.Group>
+          </Col>
+          <Col className={styles.column}>
+            <h3>Student Access Level</h3>
+            <div className={styles.content}>
+              <p>Standard</p>
+              <ul>
+                <li>Add texts to faculty-made tabs</li>
+                <li>Add sections to faculty-made tabs</li>
+              </ul>
+              <hr />
+              <p>Additional</p>
+              <div className={styles.additional}>
+                <div className="form-check">
+                  <label className="form-check-label">
+                    <input
+                      checked={
+                        plannerInfo.studentAccessLevel ===
+                          ACCESS_LEVELS.partial ||
+                        plannerInfo.studentAccessLevel === ACCESS_LEVELS.full
+                      }
+                      className="form-check-input"
+                      disabled={
+                        plannerInfo.studentAccessLevel === ACCESS_LEVELS.full
+                      }
+                      type="checkbox"
+                      name={ACCESS_LEVELS.partial}
+                      onChange={handleCheckboxChange}
+                    />
+                    Make and edit new tabs
+                  </label>
+                </div>
+                <div className="form-check">
+                  <label className="form-check-label">
+                    <input
+                      className="form-check-input"
+                      checked={
+                        plannerInfo.studentAccessLevel === ACCESS_LEVELS.full
+                      }
+                      type="checkbox"
+                      name={ACCESS_LEVELS.full}
+                      onChange={handleCheckboxChange}
+                    />
+                    Make new tabs and edit all tabs
+                  </label>
+                </div>
+              </div>
+            </div>
+          </Col>
         </Row>
-        <Row>
-          <Form.Group as={Col}>
-            <Form.Label>Required minimum word count in text section</Form.Label>
-            <Form.Control
-              type="number"
-              value={plannerInfo.minWordsCount}
-              onChange={(event) =>
-                handlePlannerUpdate(
-                  "minWordsCount",
-                  event.target.value ? parseInt(event.target.value) : ""
-                )
-              }
-            />
-          </Form.Group>
-          <Form.Group as={Col}>
-            <Form.Label>Required minimum items in checklist section</Form.Label>
-            <Form.Control
-              type="number"
-              value={plannerInfo.minChecklistItems}
-              onChange={(event) =>
-                handlePlannerUpdate(
-                  "minChecklistItems",
-                  event.target.value ? parseInt(event.target.value) : ""
-                )
-              }
-            />
-          </Form.Group>
-        </Row>
-      </Form>
-    </Container>
+      </Container>
+    </Form>
   );
 };
