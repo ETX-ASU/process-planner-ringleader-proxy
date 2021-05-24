1f25954ed7ab48da695e110dc39467ad0ec67ce7
Artur Wróbel <artur.gtg.wrobel@gmail.com>
2021-05-24 08:39:31 +0200

    fix: initial word count in text section

--- src/tool/components/Sections/Section/TextSection.js
+++ src/tool/components/Sections/Section/TextSection.js
@@ -4,33 +4,46 @@ import { calculateWordCount } from "../../../ToolUtils";
 import { USER_TYPE } from "../../../constants";
 import styles from "./Section.module.scss";
 
-export const TextSection = ({ canEdit, minWordCount, value, onChange, userType }) => {
-  const [wordCount, setWordCount] = useState(0)
+export const TextSection = ({
+  canEdit,
+  minWordCount,
+  value,
+  onChange,
+  userType,
+}) => {
+  const [wordCount, setWordCount] = useState(
+    value.length > 0 ? calculateWordCount(value) : 0
+  );
 
-  const handleChange = useCallback((event) => {
-    const text = event.target.value;
-    const wordCount = calculateWordCount(text);
+  const handleChange = useCallback(
+    (event) => {
+      const text = event.target.value;
+      const wordCount = calculateWordCount(text);
 
-    setWordCount(wordCount);
-    onChange(event);
-  }, [onChange]);
+      setWordCount(wordCount);
+      onChange(event);
+    },
+    [onChange]
+  );
 
   return canEdit ? (
     <>
-      <textarea value={value} onChange={handleChange} placeholder="Enter text here" />
+      <textarea
+        value={value}
+        onChange={handleChange}
+        placeholder="Enter text here"
+      />
       {userType === USER_TYPE.student && canEdit && (
         <p className={styles.wordCount}>
           <span>Words: {wordCount}</span>
-          {minWordCount ? (
-            <span>Required words: {minWordCount}</span>
-          ) : null}
+          {minWordCount ? <span>Required words: {minWordCount}</span> : null}
         </p>
       )}
     </>
   ) : (
     <div className={styles.textSection}>{value}</div>
   );
-}
+};
 
 TextSection.propTypes = {
   canEdit: PropTypes.bool.isRequired,
